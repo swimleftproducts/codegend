23fb689a3f80899757f75f403049f84f00fb670e
Eric Harvey <swimleft@gmail.com>
2022-02-16 18:01:42 -0800

    got map working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
+import React from 'react';
 
 import './App.css';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api'
 
 function App() {
+  const {isLoaded,loadError}= useLoadScript({
+    googleMapsApiKey: "AIzaSyAl-05QHjB4SkZgReeANCxOqKbgDwoad78"
+  })
+
+  const mapContainerStyle={
+    width:"100vw",
+    height:"100vh"
+  }
+  const center ={
+    lat:38.7422,
+    lng:-108.0690
+  }
+
+  if(loadError){
+    return("Error loading map")
+  }
+  if(!isLoaded) return "Loading Maps"
+
+
+
   return (
     <div className="App">
-       Hello!
+       <GoogleMap 
+          mapContainerStyle={mapContainerStyle}
+          zoom={10}
+          center={center}
+        >
+
+       </GoogleMap>
     </div>
   );
 }
