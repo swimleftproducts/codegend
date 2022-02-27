7450f2ffe01af505001b78c32c4dd6c7a296183e
eric <swimleft@gmail.com>
2022-02-26 17:43:21 -0800

    revising registration

--- client/src/components/RegisterCard.jsx
+++ client/src/components/RegisterCard.jsx
@@ -1,11 +1,16 @@
 import axios from 'axios'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import { validate } from 'uuid'
 
 export default function RegisterCard(props) {
   const [password,setPassword]=useState('')
   const [email,setEmail]=useState('')
   const [name, setName]=useState('')
   const [message, setMessage]=useState('')
+  const [nameMessage, setNameMessage]=useState('')
+  const [emailMessage, setEmailMessage]=useState('')
+  const [passwordMessage, setPasswordMessage]=useState('')
+
 
   const{setAuth, setShowRegister}=props
 
@@ -24,42 +29,81 @@ export default function RegisterCard(props) {
       setMessage("Registration Error")
      })
   }
+  const validateName =()=>{
+    if(name.length<=3){
+      setNameMessage("at least 2 letters")
+    }
+    if(name.length===0){
+      setNameMessage("")
+    }
+    if(name.length>3){
+      setNameMessage("")
+    }
+  }
+
+  const validatePassword =()=>{
+    if(password.length<=5){
+      setPasswordMessage("at least 6 character")
+    }
+    if(password.length===0){
+      setPasswordMessage("")
+    }
+    if(password.length>5){
+      setPasswordMessage("")
+    }
+  }
+  useEffect(() => {
+    validateName(); 
+    validatePassword();
+  },[name,password]
+  )
 
   return (
     <div className=" register card shadow-2-strong">
+     
           <div className="card-body p-4 text-center">
-            <h4 className="mb-2">Register</h4>
-
+          <div className="register-heading "> 
+            <h4 className="mb-1 ">Register</h4>
+          </div>
+            
+           
             <div className="form-outline mb-2">
+            <label className="form-label p-0 m-0 " htmlFor="typeEmailX-2">Name</label>
               <input type="email" id="typeEmailX-2" className="form-control form-control-lg"
               onChange={(e) => {
-                setName(e.target.value)    
+                setName(e.target.value) 
+                 
               }} />
-              <label className="form-label" htmlFor="typeEmailX-2">Name</label>
+              {nameMessage?<p className="error-text">{nameMessage}</p>:<p className="error-text"></p>}
+              
             </div>
 
             <div className="form-outline mb-2">
-              <input type="email" id="typeEmailX-2" className="form-control form-control-lg"
+                <label className="form-label p-0 m-0" htmlFor="typeEmailX-2">Email or username</label>
+                <input type="email" id="typeEmailX-2" className="form-control form-control-lg"
               onChange={(e) => {
                 setEmail(e.target.value)    
               }} />
-              <label className="form-label" htmlFor="typeEmailX-2">Email or username</label>
+              {emailMessage?<p className="error-text">{emailMessage}</p>:<p className="error-text"></p>}
+            
             </div>
 
-            <div className="form-outline mb-3">
+            <div className="form-outline mb-2">
+            <label className="form-label p-0 m-0" htmlFor="typePasswordX-2">Password</label>
               <input type="password" id="typePasswordX-2" className="form-control form-control-lg"
               onChange={(e) => {
                 setPassword(e.target.value)    
               }} />
-              <label className="form-label" htmlFor="typePasswordX-2">Password</label>
+              {passwordMessage?<p className="error-text">{passwordMessage}</p>:<p className="error-text"></p>}
+              
             </div>
 
-            <button className="btn btn-primary btn-med btn-block" type="submit"
+            <button className=" mt-1 btn register-form-btn btn-med btn-block" type="submit"
              onClick={(e) => {
               e.preventDefault()
               onSubmit();
             }}>Sign Up</button>
-              {message?<p>{message}</p>:null}
+              {message?<p className="error-text-main">{message}</p>:null}
           </div>
         </div>
   )
