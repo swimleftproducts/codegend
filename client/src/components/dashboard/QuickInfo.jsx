9106e0cee09fe1a8c8a25a4a621c702f1780583e
Mat C <57735350+m4th3w561@users.noreply.github.com>
2022-03-06 08:38:46 +0900

    fixing quickinfo

--- client/src/components/dashboard/QuickInfo.jsx
+++ client/src/components/dashboard/QuickInfo.jsx
@@ -1,16 +1,24 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { UserStatsContext } from '../UserStatsContext'
 
 function QuickInfo() {
-  const {info}= useContext(UserStatsContext)
+  const { info } = useContext(UserStatsContext)
 
-  const renderInfo =()=>{
-    if(info.userStats){
-      return(
-        <p> Total Visits {info.userStats.cumulativeData.data[5]} / {info.userStats.numLocations}</p>
+  const renderInfo = () => {
+    if (info.userStats) {
+      return (
+        <div className='container-fluid'>
+          <div className='quick-info-label'>
+            <span class="bi-award-fill"></span>
+            <h6>Total Visits</h6>
+          </div>
+
+          <p>{info.userStats.cumulativeData.data[5]} / {info.userStats.numLocations}</p>
+        </div>
+
       )
-    }else{
-      return("Loading")
+    } else {
+      return ("Loading")
     }
   }
 
