4e67b276935b47ccdee9ebe6f6e37c6493eca0a7
Eric Harvey <swimleft@gmail.com>
2022-03-09 19:57:24 -0800

    added color changing markers

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,96 +1,118 @@
-import React, { useEffect,useContext} from 'react'
+import React, { useEffect, useContext } from 'react';
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
-} from '@react-google-maps/api'
+  MarkerClusterer,
+} from '@react-google-maps/api';
 import LocationDetail from './LocationDetail';
 import { LocationContext } from './LocationContext';
-import {DisplayContext} from './DisplayContext'
+import { DisplayContext } from './DisplayContext';
 import axios from 'axios';
 
-
 function Map(props) {
-  const { selectedLocation, setSelectedLocation, mapCenter,  locations, setLocations, infoBoxOffset,setInfoBoxOffset} = useContext(LocationContext)
-  const{setShowDashboard,setShowDatePicker,setShowRecentVisitors} = useContext(DisplayContext)
+  const {
+    selectedLocation,
+    setSelectedLocation,
+    mapCenter,
+    locations,
+    setLocations,
+    infoBoxOffset,
+    setInfoBoxOffset,
+  } = useContext(LocationContext);
+  const { setShowDashboard, setShowDatePicker, setShowRecentVisitors } =
+    useContext(DisplayContext);
 
+  const { auth } = props;
 
-  const {auth}=props
+  useEffect(() => {
+    //from LocationContext
+    getLocationData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  useEffect(() => {  
-  //from LocationContext
-   getLocationData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-
-  async function getLocationData(){
-    let response = await axios('/api/geo/markers')
-    let markers = await response.data
-    setLocations(markers)
+  async function getLocationData() {
+    let response = await axios('/api/geo/markers');
+    let markers = await response.data;
+    setLocations(markers);
   }
 
-   const {isLoaded,loadError}= useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-  })
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
 
-  const mapContainerStyle={
-    width:"100vw",
-    height:"100vh"
+  const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+  };
+
+  const options = {
+    streetViewControl: false,
+    fullscreenControl: false,
+    disableAutoPan: true,
+    clickableIcons: false,
+  };
+
+  if (loadError) {
+    return 'Error loading map';
   }
-
-  const options ={
-   streetViewControl: false,
-   fullscreenControl: false,
-   disableAutoPan: true,
-   clickableIcons: false
-  }
-
-
-  if(loadError){
-    return("Error loading map")
-  }
-  if(!isLoaded) return "Loading Maps"
+  if (!isLoaded) return 'Loading Maps';
   return (
-    <GoogleMap 
-    mapContainerStyle={mapContainerStyle}
-    zoom={10}
-    center={mapCenter}
-    options={options}
-  >
-    {locations.map((location) => {
-      
-        return <Marker key={location._id} position={{
-          lat:location.lat, 
-          lng:location.lng
-        }} 
-        onClick={() => {
-          setShowDashboard(false)
-          setSelectedLocation(location)
-        }}/>
-      }
-    )}
-    {selectedLocation ?(<InfoWindow 
-      options={{pixelOffset: new window.google.maps.Size(0,infoBoxOffset),noRedraw: true}}
-       position={{
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      zoom={10}
+      center={mapCenter}
+      options={options}
+    >
+      {locations.map((location) => {
+        let visited =false
+        location.visitedBy.forEach((visitor) => {
         
-        lat:selectedLocation.lat, 
-        lng:selectedLocation.lng
-       }}
-       
-       onCloseClick={() => {
+          if(auth.id ===visitor.userId){
+             visited=true;
+          }
+          return 0
+        })
+        return (
+          <Marker
          
-         setSelectedLocation(null)
-         setShowDatePicker(false)
-         setShowRecentVisitors(true)
-         setInfoBoxOffset(-40)
-      }}
-      >
-       <LocationDetail auth={auth}/>
-      </InfoWindow>):null}
-      
+        
+          key={location._id}
+            position={{
+              lat: location.lat,
+              lng: location.lng,
+            }}
+            icon={`${visited?"http://maps.google.com/mapfiles/ms/icons/blue.png":'http://maps.google.com/mapfiles/ms/icons/red.png'}`}
+            onClick={() => {
+              setShowDashboard(false);
+              setSelectedLocation(location);
+            }}
+          />
+        );
+      })}
+      {selectedLocation ? (
+        <InfoWindow
+          options={{
+            pixelOffset: new window.google.maps.Size(0, infoBoxOffset),
+            noRedraw: true,
+          }}
+          position={{
+            lat: selectedLocation.lat,
+            lng: selectedLocation.lng,
+          }}
+          onCloseClick={() => {
+            setSelectedLocation(null);
+            setShowDatePicker(false);
+            setShowRecentVisitors(true);
+            setInfoBoxOffset(-40);
+          }}
+        >
+          <LocationDetail auth={auth} />
+        </InfoWindow>
+      ) : null}
     </GoogleMap>
-  )
+  );
 }
 
-export default Map
+export default Map;
