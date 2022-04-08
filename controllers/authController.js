e21c395657d9421d440d6a587b17b202ba5184c9
eric <swimleft@gmail.com>
2022-04-07 20:20:48 -0700

    debugging email

--- controllers/authController.js
+++ controllers/authController.js
@@ -142,7 +142,7 @@ module.exports = {
     // build link
 
     //send email to user that includes a link with the token  
-    sendEmail(user.email,"GeoCache - PW Reset",{rootURL:process.env.ROOTURL,token:resetToken},"./templates/welcome.handlebars")
+    await sendEmail(user.email,"GeoCache - PW Reset",{rootURL:process.env.ROOTURL,token:resetToken},"./templates/welcome.handlebars")
      res.send({message: "hi!"})
   },
 
