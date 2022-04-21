c27e485a3126fc7367e481b90d3181b814c414b3
Eric Harvey <swimleft@gmail.com>
2022-04-21 16:08:21 -0700

    switched to sendgrid

--- controllers/utils.js
+++ controllers/utils.js
@@ -2,49 +2,17 @@ const nodemailer= require("nodemailer");
 const handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
+//sendgrid setup
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
-// const oauth2Client = new OAuth2(
-//   process.env.CLIENT_ID,
-//   process.env.CLIENT_SECRET,
-//   "https://developers.google.com/oauthplayground"
-// )
-// oauth2Client.setCredentials({
-//      refresh_token: "1//04tOynYjaztFXCgYIARAAGAQSNwF-L9IrgA7dYGn9kpmZLDG1xFokTy7BnsPIlUDIm9cErH81X3Bd4kGTNxnanh9UTiK5bkd9_LQ"
-// });
-// const accessToken = oauth2Client.getAccessToken()
-// console.log(process.env.REFRESH_TOKEN)
+
 
 const sendEmail = async(email,subject,payload,template)=>{
   try{
-    console.log("password",process.env.EMAIL_PASSWORD)
-    console.log("uname",process.env.EMAIL_USERNAME)
+   
 
-    // **** old working code
-    // const transporter = nodemailer.createTransport({
-    //   host: process.env.EMAIL_HOST,
-    //   port: 465,
-    //   auth:{
-    //     user: process.env.EMAIL_USERNAME,
-    //     pass: process.env.EMAIL_PASSWORD
-    //   }
-    // })
-    // const transporter= nodemailer.createTransport({
-    //  service: "gmail",
-    //  auth: {
-    //       type: "OAuth2",
-    //       user: "swimleft@gmail.com", 
-    //       clientId: process.env.CLIENT_ID,
-    //       clientSecret: process.env.CLIENT_SECRET,
-    //       refreshToken: "1//04tOynYjaztFXCgYIARAAGAQSNwF-L9IrgA7dYGn9kpmZLDG1xFokTy7BnsPIlUDIm9cErH81X3Bd4kGTNxnanh9UTiK5bkd9_LQ",
-    //       accessToken: accessToken
-    //       },
-    //       tls: {
-    //         rejectUnauthorized: false
-    //       }
-    //   });
 
 
     // const source = fs.readFileSync(path.join(__dirname,template),"utf8");
@@ -58,15 +26,25 @@ const sendEmail = async(email,subject,payload,template)=>{
     //   }
     // }
 
-    // transporter.sendMail(options(),(error,info)=>{
-    //   if(error){
-    //     console.log(error)
-    //     return error
-    //   }else{
-    //     console.log("email sent")
-    //     return res.status(200).json({success:true})
-    //   }
-    // })
+
+    const source = fs.readFileSync(path.join(__dirname,template),"utf8");
+    const compiledTemplate = handlebars.compile(source);
+
+    const msg = {
+      to: 'swimleft@gmail.com', // Change to your recipient
+      from: process.env.SENDGRID_EMAIL, // Change to your verified sender
+      subject: 'SarCache PW Reset',
+      
+      html: compiledTemplate(payload)
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
 
 
   }catch(err){
