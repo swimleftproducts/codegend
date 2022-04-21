cd13c2cc1fbbae94febd9ebfc1d2e774dbd8f718
Eric Harvey <swimleft@gmail.com>
2022-04-21 15:39:14 -0700

    removed o auth

--- controllers/utils.js
+++ controllers/utils.js
@@ -6,16 +6,16 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 
 
-const oauth2Client = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
-)
-oauth2Client.setCredentials({
-     refresh_token: "1//04tOynYjaztFXCgYIARAAGAQSNwF-L9IrgA7dYGn9kpmZLDG1xFokTy7BnsPIlUDIm9cErH81X3Bd4kGTNxnanh9UTiK5bkd9_LQ"
-});
-const accessToken = oauth2Client.getAccessToken()
-console.log(process.env.REFRESH_TOKEN)
+// const oauth2Client = new OAuth2(
+//   process.env.CLIENT_ID,
+//   process.env.CLIENT_SECRET,
+//   "https://developers.google.com/oauthplayground"
+// )
+// oauth2Client.setCredentials({
+//      refresh_token: "1//04tOynYjaztFXCgYIARAAGAQSNwF-L9IrgA7dYGn9kpmZLDG1xFokTy7BnsPIlUDIm9cErH81X3Bd4kGTNxnanh9UTiK5bkd9_LQ"
+// });
+// const accessToken = oauth2Client.getAccessToken()
+// console.log(process.env.REFRESH_TOKEN)
 
 const sendEmail = async(email,subject,payload,template)=>{
   try{
@@ -31,42 +31,42 @@ const sendEmail = async(email,subject,payload,template)=>{
     //     pass: process.env.EMAIL_PASSWORD
     //   }
     // })
-    const transporter= nodemailer.createTransport({
-     service: "gmail",
-     auth: {
-          type: "OAuth2",
-          user: "swimleft@gmail.com", 
-          clientId: process.env.CLIENT_ID,
-          clientSecret: process.env.CLIENT_SECRET,
-          refreshToken: "1//04tOynYjaztFXCgYIARAAGAQSNwF-L9IrgA7dYGn9kpmZLDG1xFokTy7BnsPIlUDIm9cErH81X3Bd4kGTNxnanh9UTiK5bkd9_LQ",
-          accessToken: accessToken
-          },
-          tls: {
-            rejectUnauthorized: false
-          }
-      });
+    // const transporter= nodemailer.createTransport({
+    //  service: "gmail",
+    //  auth: {
+    //       type: "OAuth2",
+    //       user: "swimleft@gmail.com", 
+    //       clientId: process.env.CLIENT_ID,
+    //       clientSecret: process.env.CLIENT_SECRET,
+    //       refreshToken: "1//04tOynYjaztFXCgYIARAAGAQSNwF-L9IrgA7dYGn9kpmZLDG1xFokTy7BnsPIlUDIm9cErH81X3Bd4kGTNxnanh9UTiK5bkd9_LQ",
+    //       accessToken: accessToken
+    //       },
+    //       tls: {
+    //         rejectUnauthorized: false
+    //       }
+    //   });
 
 
-    const source = fs.readFileSync(path.join(__dirname,template),"utf8");
-    const compiledTemplate = handlebars.compile(source);
-    const options = () => {
-      return{
-        from: process.env.EMAIL_USERNAME,
-        to: email,
-        subject: subject,
-        html: compiledTemplate(payload)
-      }
-    }
+    // const source = fs.readFileSync(path.join(__dirname,template),"utf8");
+    // const compiledTemplate = handlebars.compile(source);
+    // const options = () => {
+    //   return{
+    //     from: process.env.EMAIL_USERNAME,
+    //     to: email,
+    //     subject: subject,
+    //     html: compiledTemplate(payload)
+    //   }
+    // }
 
-    transporter.sendMail(options(),(error,info)=>{
-      if(error){
-        console.log(error)
-        return error
-      }else{
-        console.log("email sent")
-        return res.status(200).json({success:true})
-      }
-    })
+    // transporter.sendMail(options(),(error,info)=>{
+    //   if(error){
+    //     console.log(error)
+    //     return error
+    //   }else{
+    //     console.log("email sent")
+    //     return res.status(200).json({success:true})
+    //   }
+    // })
 
 
   }catch(err){
