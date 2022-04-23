63267f095b30304c69a5531fbe60dfeda38624b0
Eric Harvey <swimleft@gmail.com>
2022-04-22 19:17:01 -0700

    fixed email in rester

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,59 +1,34 @@
-const nodemailer= require("nodemailer");
-const handlebars = require("handlebars");
-const fs = require("fs");
-const path = require("path");
+const nodemailer = require('nodemailer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
 //sendgrid setup
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
-
-
-const sendEmail = async(email,subject,payload,template)=>{
-  try{
-   
-
-
-
-    // const source = fs.readFileSync(path.join(__dirname,template),"utf8");
-    // const compiledTemplate = handlebars.compile(source);
-    // const options = () => {
-    //   return{
-    //     from: process.env.EMAIL_USERNAME,
-    //     to: email,
-    //     subject: subject,
-    //     html: compiledTemplate(payload)
-    //   }
-    // }
-
-
-    const source = fs.readFileSync(path.join(__dirname,template),"utf8");
+const sendEmail = async (email, subject, payload, template) => {
+  try {
+    const source = fs.readFileSync(path.join(__dirname, template), 'utf8');
     const compiledTemplate = handlebars.compile(source);
 
     const msg = {
-      to: 'swimleft@gmail.com', // Change to your recipient
+      to: email, // Change to your recipient
       from: process.env.SENDGRID_EMAIL, // Change to your verified sender
       subject: 'SarCache PW Reset',
-      
-      html: compiledTemplate(payload)
-    }
+
+      html: compiledTemplate(payload),
+    };
     sgMail
       .send(msg)
       .then(() => {
-        console.log('Email sent')
+        console.log('Email sent');
       })
       .catch((error) => {
-        console.error(error)
-      })
-
-
-  }catch(err){
-    console.log(err)
+        console.error(error);
+      });
+  } catch (err) {
+    console.log(err);
   }
+};
 
-
-
-
-}
-
-module.exports=sendEmail;
+module.exports = sendEmail;
